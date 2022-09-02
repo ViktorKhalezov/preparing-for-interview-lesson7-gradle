54a8bd0cd41181d3c12e7d29f1741c06a5ee8716
Viktor Khalezov <viktor-khalezov@mail.ru>
2022-09-02 15:15:31 +0300

    Initial commit

---
+++ src/main/java/ru/gb/PreparingForInterviewLesson7GradleApplication.java
@@ -0,0 +1,13 @@
+package ru.gb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreparingForInterviewLesson7GradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreparingForInterviewLesson7GradleApplication.class, args);
+	}
+
+}
